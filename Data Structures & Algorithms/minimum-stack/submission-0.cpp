cf7b4cc1fa7d9cfd4aaa7bb2fddc702da0deb379
Aadi Deshmukh <aadi14des@gmail.com>
2026-06-02 21:51:19 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,37 @@
+class MinStack {
+public:
+    stack<int> s;
+
+    MinStack() {
+        
+        
+    }
+    
+    void push(int val) {
+        s.push(val);   
+    }
+    
+    void pop() {
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        stack<int> temp; 
+        int mini= s.top();
+        while(s.size()){
+            mini=min(mini,s.top());
+            temp.push(s.top());
+            s.pop();
+        }
+
+        while(temp.size()){
+            s.push(temp.top());
+            temp.pop();
+        }
+        return mini;
+    }
+};
