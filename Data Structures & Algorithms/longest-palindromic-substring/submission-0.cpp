c340277eca6028af9d77bc9a9b73e730d511b77f
Aadi Deshmukh <aadi14des@gmail.com>
2026-08-13 20:13:54 +0530

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string ans="";
+        int anslen=0, ansid=0;
+
+            for(int i=0;i<s.length();i++){
+                //length odd
+                int l=i, r=i;
+                while(l>=0 && r< s.size()&& s[l]==s[r]){
+                    if(r-l+1>anslen){
+                        ansid=l;
+                        anslen= r-l+1;
+                    }
+                    l--;
+                    r++;
+                }
+
+                l=i;
+                r=i+1;
+                while(l>=0 && r< s.size() && s[l]==s[r]){
+                    if(r-l+1>anslen){
+                        ansid=l;
+                        anslen=r-l+1;
+                    }
+                    l--;
+                    r++;
+                }
+            }
+        return s.substr(ansid, anslen);
+    }
+};
