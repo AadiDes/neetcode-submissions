d1971e0d8d3a29385c6d0331d965e568d99cbe5a
Aadi Deshmukh <aadi14des@gmail.com>
2026-08-17 10:37:48 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<vector<int>> ans;
+    void dfs(vector<int>& nums, int i, vector<int>& curr) {
+        if (i >= nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+        // include nums[i]
+        curr.push_back(nums[i]);
+        dfs(nums, i + 1, curr);
+        // dont include nums[i];
+        curr.pop_back();
+        dfs(nums, i + 1, curr);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> curr;
+        dfs(nums, 0, curr);
+        return ans;
+    }
+};
