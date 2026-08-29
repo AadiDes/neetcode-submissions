bfd3b01ba20e1524777ad28f353bf2ffa7e9f15e
Aadi Deshmukh <aadi14des@gmail.com>
2026-08-29 15:29:17 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<string> res;
+    vector<string> digToChar{ "","", "abc", "def", "ghi", "jkl", "mno", "qprs", "tuv", "wxyz"};
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return res;
+        backtrack(0, "", digits);
+        return res;
+    }
+
+    void backtrack(int i, string cur, string& digits) {
+        if (cur.size() == digits.size()) {
+            res.push_back(cur);
+            return;
+        }
+        string chars = digToChar[digits[i] - '0'];
+        for (char c : chars) {
+            backtrack(i + 1, cur+c, digits);
+        }
+    }
+};
